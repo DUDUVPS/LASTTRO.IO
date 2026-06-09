1be77a24611f18091ff3e8665cdd5beca89aca01
DUDUVPS <souzadeveduardo@gmail.com>
2026-06-09 15:23:31 -0300

    prepara lasttro como app android pwa

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'lasttro-pwa-v1';
+const APP_SHELL = [
+  '/',
+  '/app',
+  '/index.html',
+  '/apresentacao.html',
+  '/styles.css',
+  '/presentation.css',
+  '/app.js',
+  '/manifest.webmanifest',
+  '/assets/logo-nome.png',
+  '/assets/logotipo.ico',
+  '/assets/favicon-32.png',
+  '/assets/app-icon-192.png',
+  '/assets/app-icon-512.png',
+  '/favicon.ico'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin || requestUrl.pathname.startsWith('/api/')) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('/app')))
+  );
+});
--- server.js
+++ server.js
@@ -75,6 +75,8 @@ const mimeTypes = {
   '.css': 'text/css; charset=utf-8',
   '.js': 'application/javascript; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
+  '.webmanifest': 'application/manifest+json; charset=utf-8',
+  '.png': 'image/png',
   '.ico': 'image/x-icon'
 };
 
